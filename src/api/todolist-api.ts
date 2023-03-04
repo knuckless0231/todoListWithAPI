0ae9b38bbb41455afa85acf43c19c00e20b36b2a
Kostya <110088843+knuckless0231@users.noreply.github.com>
2023-03-04 21:45:03 +0200

    unit test task reducer todo reducer (#2)
    * 22:40
    
    * unit tests for todoReducer and taskReducer
    
    ---------
    
    Co-authored-by: Kostya Kokhanov <knuckostya@gmail.com>

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {TaskType} from "../state/tasks-reducer";
 
 
 const instance = axios.create({
@@ -29,6 +30,7 @@ export const todoListApi = {
         return instance.get<GetTaskResponseType>(`todo-lists/${todoID}/tasks`)
     },
     createTasks(todoID: string, title: string) {
+
         return instance.post<ResponseTasksType<{item:TaskType}>>(`todo-lists/${todoID}/tasks`, {title})
     },
     updateTasks(todoID: string, taskID: string, title: string) {
@@ -58,32 +60,6 @@ type TodoListResponseType<T = {}> = {
 
 // --------------------------------------------------- tasks type
 
-export enum TaskStatuses {
-    New = 0,
-    InProgress = 1,
-    Completed = 2,
-    Draft =3
-}
-export enum TaskPriorities {
-    Low = 0,
-    Middle = 1,
-    Hi = 2,
-    Urgently = 3,
-    Later = 4
-}
-
-export type TaskType = {
-    id: string;
-    title: string;
-    description?: any;
-    todoListId: string;
-    order: number;
-    status: TaskStatuses;
-    priority: TaskPriorities;
-    startDate?: any;
-    deadline?: any;
-    addedDate: string;
-}
 
 export type GetTaskResponseType = {
     items: TaskType[];
